cbc9c714dc2be5ea793d8cf1cb305f1ce4b6ab2b
Vladyslav Kekukh <vladyslavkekukh@MacBook-Pro-Vladyslav.local>
2020-04-14 15:52:48 +0200

    init backend

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const testRoutes = require("./routes/test");
+
+mongoose
+  .connect(
+    "mongodb+srv://momsHelperUser:iap2nY5iQqeNXs@cluster0-pjau0.mongodb.net/momshelper?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connection is up and running");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+ app.use("/api/test", testRoutes);
+
+module.exports = app;
